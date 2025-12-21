6bcc9bdfac6eb42348c253395a4a67034bdf0cf2
priya-singh27 <priya.beingdev@gmail.com>
2025-12-21 20:04:59 +0530

    Enhance file listing with color coding for files, directories, and symbolic links;

--- ls-command.js
+++ ls-command.js
@@ -2,17 +2,51 @@ const fs = require('fs/promises');
 
 const listFiles = async (dir) => {
     const files = await fs.readdir(dir, { encoding: 'utf-8', withFileTypes: true, recursive: false });
-    console.log(files);
+    files.forEach(async (dirent) => {
+        try {
+            if (dirent.isFile()) {
+                dirent.name = `\x1B[33m${dirent.name}\x1B[0m`;
+            }
+            else if (dirent.isDirectory()) {
+                dirent.name = `\x1B[31m${dirent.name}\x1B[0m`;
+            } else if (dirent.isSymbolicLink()) {
+                dirent.name = `\x1B[34m${dirent.name}\x1B[0m`;
+            }
+
+        } catch (err) {
+            console.log(err);
+        }
+    });
+    return files;
 }
 
 const fileTypes = async (files) => {
     files.forEach(async (file) => {
-        const stat = await fs.stat(file);
-        console.log(`${file}:`, stat);
+        try {
+            const isFile = file.isFile();
+            if (isFile) {
+
+            }
+            else {
+                const isDir = file.isDirectory();
+                if (isDir) {
+                    
+                }
+            }
+
+            // const filePath =`${file.parentPath}/${file.name}`
+            // const stat = await fs.stat(filePath);
+            // console.log(`${file.name}:`, stat);
+
+        } catch (err) {
+            console.log(`no stat for ${file}`);
+        }
+        
     });
 }
 
 module.exports = {
     listFiles,
-    fileTypes
+    fileTypes,
+    
 }
--- organizer.js
+++ organizer.js
@@ -2,11 +2,23 @@ const process = require('process');
 const {listFiles, fileTypes} = require('./ls-command');
 
 const folderName = process.argv[2];
-const args = process.argv.slice[3];
+const args = process.argv.slice(3);
 
 const ls = async (folderName) => {
-    const files = await listFiles(folderName);
-    
+    const filesArray = await listFiles(folderName);
+    filesArray.forEach((direnObj) => {
+        console.log(direnObj.name);
+    })
+
+
+    if (args) {
+        args.forEach(async (arg) => {
+            if (arg === '--show-types') {
+                // console.log(files);
+                // await fileTypes(files);
+            }
+        })
+    }
 }
 ls(folderName);
 
